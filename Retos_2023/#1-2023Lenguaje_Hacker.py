e5462b935448fd3d0e722ec9a2f20306a249a4b5
mikianes <mikianes@gmail.com>
2023-01-10 12:27:55 +0100

    Semana_uno

---
+++ Retos_2023/#1-2023Lenguaje_Hacker.py
@@ -0,0 +1,35 @@
+"""
+/*
+ * Escribe un programa que reciba un texto y transforme lenguaje natural a
+ * "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
+ *  se caracteriza por sustituir caracteres alfanuméricos.
+ * - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/) 
+ *   con el alfabeto y los números en "leet".
+ *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
+ */
+"""
+
+diccionario = {"a":"4", "b":"l3", "c":"[", "d":")", "e":"3", "f":"|=", "g":"&", "h":"#", "i":"1", "j":",_|", "k":">|", 
+                   "l":"1", "m":"/\/\"", "n":"^/", "o":"0", "p":"|*", "q":"(_,)", "r":"l2", "s":"5", "t":"7", "u":"(_)", 
+                   "v":"\/", "w":"\/\/", "x":"><", "y":"j", "z":"2",
+                   "1":"L", "2":"R", "3":"E", "4":"A", "5":"S", "6":"b", "7":"T", "8":"B", "9":"g", "0":"o", " ":" "}
+
+
+def datos(a):
+    print ("Cadena origen:",a)
+    origen = a
+    destino = ""
+    
+    for letra in origen:
+        destino += diccionario[letra]
+        
+    print("Resultado:", destino)
+
+#Introducir Palabra o Frase
+def inicial():
+    txt = input ("Texto a convertir:")
+    texto = txt.lower()
+    datos(texto)
+
+#Llamada inicial
+inicial()

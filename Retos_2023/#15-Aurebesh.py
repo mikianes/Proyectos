b6ca2444a73f458eadcc221da6a1c090a11eb0a2
mikianes <mikianes@gmail.com>
2023-04-19 15:53:49 +0200

    Version 1

--- Retos_2023/#15-Aurebesh.py
+++ Retos_2023/#15-Aurebesh.py
@@ -1,34 +1,55 @@
+"""/*
+ * Crea una función que sea capaz de transformar Español al lenguaje básico del universo
+ * Star Wars: el "Aurebesh".
+ * - Puedes dejar sin transformar los caracteres que no existan en "Aurebesh".
+ * - También tiene que ser capaz de traducir en sentido contrario.
+ *  
+ * ¿Lo has conseguido? Nómbrame en twitter.com/mouredev y escríbeme algo en Aurebesh.
+ *
+ * ¡Que la fuerza os acompañe!
+ */
+"""
+
+
 # Import the tkinter module
-import tkinter
+from tkinter import messagebox, ttk
+from tkinter.messagebox import showinfo
+import tkinter as tk
  
 # Creating the GUI window.
-window = tkinter.Tk()
-window.title("Welcome to geeksforgeeks")
-window.geometry("800x100")
- 
-# Creating our text widget.
-sample_text = tkinter.Entry(window)
-sample_text.pack()
- 
-# Creating the function to set the text
-# with the help of button
-def set_text_by_button():
- 
-    # Delete is going to erase anything
-    # in the range of 0 and end of file,
-    # The respective range given here
-    sample_text.delete(0,"end")
-     
-    # Insert method inserts the text at
-    # specified position, Here it is the
-    # beginning
-    sample_text.insert(0, "Text set by button")
- 
-# Setting up the button, set_text_by_button()
-# is passed as a command
-set_up_button = tkinter.Button(window, height=1, width=10, text="Set",
-                    command=set_text_by_button)
- 
-set_up_button.pack()
+window = tk.Tk()
+window.title("Aurebesh Translator")
+window.geometry("400x150")
+
+# Labels
+w = tk.Label(window, text="Aurebesh Translator")
+w.grid(row=0,column=6)
+
+selected_languaje_A = tk.StringVar()
+
+comboA = ttk.Combobox(state="readonly",
+    values=["Aurebesh", "Castellano"],textvariable=selected_languaje_A
+    )
+comboA.grid(row=6,column=4,padx=10)
+#comboA.current(0)
+
+def set_lenguaje(event):
+    
+    lenguaje = selected_languaje_A.get()
+    if lenguaje == "Castellano":
+        result_text.config(font="Times")
+        result_text.config(width=30)
+        result_text.grid(row=6,column=6,padx=10)
+    else:
+        result_text.config(font="Aurebesh")
+        result_text.config(width=30)
+        result_text.grid(row=6,column=6,padx=10)
+    
+
+comboA.bind('<<ComboboxSelected>>', lambda _: set_lenguaje(selected_languaje_A.get()))
+
+result_text = tk.Entry(window,width=30)
+result_text.grid(row=6,column=6,padx=10)
+
  
 window.mainloop()

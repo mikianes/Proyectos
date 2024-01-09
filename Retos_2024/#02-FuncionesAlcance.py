99deba51fc8fe87fb2ed6a0cd166019fef93a5d0
mikianes <mikianes@gmail.com>
2024-01-09 16:50:10 +0100

    Version 1

---
+++ Retos_2024/#02-FuncionesAlcance.py
@@ -0,0 +1,55 @@
+
+
+""" * - Crea ejemplos de funciones básicas que representen las diferentes
+ *   posibilidades del lenguaje:
+ *   Sin parámetros ni retorno, con uno o varios parámetros, con retorno...
+ * - Comprueba si puedes crear funciones dentro de funciones.
+ * - Utiliza algún ejemplo de funciones ya creadas en el lenguaje.
+ * - Pon a prueba el concepto de variable LOCAL y GLOBAL.
+ * - Debes hacer print por consola del resultado de todos los ejemplos.
+ *   (y tener en cuenta que cada lenguaje puede poseer más o menos posibilidades)
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea una función que reciba dos parámetros de tipo cadena de texto y retorne un número.
+ * - La función imprime todos los números del 1 al 100. Teniendo en cuenta que:
+ *   - Si el número es múltiplo de 3, muestra la cadena de texto del primer parámetro.
+ *   - Si el número es múltiplo de 5, muestra la cadena de texto del segundo parámetro.
+ *   - Si el número es múltiplo de 3 y de 5, muestra las dos cadenas de texto concatenadas.
+ *   - La función retorna el número de veces que se ha impreso el número en lugar de los textos.
+ *
+ * Presta especial atención a la sintaxis que debes utilizar en cada uno de los casos.
+ * Cada lenguaje sigue una convenciones que debes de respetar para que el código se entienda. 
+ * Números múltiplo de 3: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99
+ * Números multiplo de 5: 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100 
+ * Números que no son múltiplo de 3 y 5: 40
+"""
+
+
+def extra(tres: str, cinco: str) -> int:
+
+    numbers = 0
+
+    for i in range(1, 101, 1):
+        three = i % 3
+        five = i % 5
+
+        if three == 0 and five == 0:
+            print(f" numero :{i}={tres+cinco}")
+        elif three == 0:
+            print(f" numero :{i}={tres}")
+        elif five == 0:
+            print(f" numero :{i}={cinco}")
+        else:
+            numbers += 1
+
+    return numbers
+
+
+def main():
+
+    count = extra("tres", "cinco")
+    print(f" Hay {count} numeros que no son multiplos de 3 o 5")
+
+
+if __name__ == "__main__":
+    main()
